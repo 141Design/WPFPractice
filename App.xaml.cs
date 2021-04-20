c5f9e23ae8096c2ce5c17a17204b030302cea593
141Design <141.atlas@gmail.com>
2021-04-20 22:55:19 +0900

    プロジェクト ファイルを追加します。

---
+++ App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace InspectionSequencer
+{
+    using WPFPractice.ViewModels;
+    using WPFPractice.Views;
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var w = new MainView();
+            var vm = new MainViewModel();
+
+            //ViewModelをデータコンテキストに指定
+            w.DataContext = vm;
+
+
+            w.Show();
+
+        }
+    }
+}
